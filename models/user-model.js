fc325021e1d4ef62e309b40bfeee405c58b6f560
naguire2018104 <naguirre-2018104@kinal.edu.gt>
2021-04-23 13:40:56 -0600

    Modelo Usuario

--- models/user-model.js
+++ models/user-model.js
@@ -1,1 +1,18 @@
 "use strict"
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var userSchema = Schema({
+    username: String,
+    password: String,
+    email: String,
+    name: String,
+    lastname: String,
+    role: {type: String, default: "ROLE_CLIENT"},
+    reservations: [],
+    invoices: [],
+    history: []
+});
+
+module.exports = mongoose.model("user",userSchema);
