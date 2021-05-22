7cf652a3ef35fe99f75d188dba118642e44ea73d
naguire2018104 <naguirre-2018104@kinal.edu.gt>
2021-05-21 19:28:09 -0600

    Room

--- models/room-model.js
+++ models/room-model.js
@@ -7,6 +7,7 @@ var roomSchema = Schema({
     name: String,
     available: { type: Boolean, default: true },
     price_for_day: Number,
+    available_day: { type: Date, default: null}
 });
 
 module.exports = mongoose.model("room", roomSchema);
